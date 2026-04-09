413c63bcefdc07866f2db5110e8782ef777eaa62
Jayani0101 <jayanilak07@gmail.com>
2026-04-09 20:02:47 +0530

    Created custom registration form with email field

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,1 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs['class'] = 'form-control'
