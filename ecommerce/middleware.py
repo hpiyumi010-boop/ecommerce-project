8a84c479fb6bd2c5c501136314ecbaa24769c47b
Jayani0101 <jayanilak07@gmail.com>
2026-04-09 20:48:07 +0530

    Updated CSRF middleware to only disable checks when DEBUG=True

--- ecommerce/middleware.py
+++ ecommerce/middleware.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.utils.deprecation import MiddlewareMixin
 
 class DisableCSRF(MiddlewareMixin):
-    """Disable CSRF verification for all requests (development only)"""
+    """Disable CSRF verification only when DEBUG = True (development only)"""
     def process_request(self, request):
-        setattr(request, '_dont_enforce_csrf_checks', True)
+        if settings.DEBUG:
+            setattr(request, '_dont_enforce_csrf_checks', True)
