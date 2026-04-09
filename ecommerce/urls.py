d854ff084dcdb22bfcdc14a6a015e79048aa481b
Jayani0101 <jayanilak07@gmail.com>
2026-04-09 14:44:05 +0530

    Disabled shop URL routing to resolve root URL conflict
    Comment out the shop URL path for optional products.

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include   # <-- add 'include'
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('myapp.urls')),     # <-- your UI and auth
-    path('shop/', include('shop.urls')), # <-- Member 1's products (optional, but keep)
+    #path('shop/', include('shop.urls')), # <-- Member 1's products (optional, but keep)
 ]
