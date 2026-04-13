1cd27b59ee0df6a74d3bef5edfc163b7564f80f1
Jayani0101 <jayanilak07@gmail.com>
2026-04-14 00:20:57 +0530

    Delete Clothing-Shop-master directory

--- Clothing-Shop-master/shop/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Category, Product, Cart, Order
-
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Cart)
-admin.site.register(Order)
